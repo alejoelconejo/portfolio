eb075c6a75989bf9b8439e0367a5451404f6a2af
Alejo Janin <alejojanin@gmail.com>
2022-11-30 07:11:23 -0300

    Improve styles

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,15 @@
 module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: { DEFAULT: '#f432cf', hover: '#dd2cba' },
+        secondary: '#a39bf9',
+        darkGray: '#191919',
+        background: '#222222',
+      },
+    },
+
     fontFamily: {
       sans: ['Inter', 'ui-sans-serif', 'system-ui'],
     },
